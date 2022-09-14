662f3483e062d9af0bae12822a4c8cd27d0a670a
xiayu <xiayu19950417@163.com>
2022-09-14 18:07:18 +0800

    Build vue framework

---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,64 @@
+<script setup>
+    defineProps({
+      msg: {
+        type: String,
+        required: true
+      }
+    })
+    </script>
+    
+    <template>
+      <header class="header-bar">
+        <img alt="logo" class="logo" src="@/assets/logo.svg" />
+
+        <nav class="header-nav">
+            <RouterLink class="header-link" to="/">global</RouterLink>
+            <RouterLink class="header-link" to="/projects">projects</RouterLink>
+        </nav>
+      </header>
+    </template>
+    
+    <style scoped>
+    .header-bar{
+        display: flex;
+        width:100%;
+        height: 3.5rem;
+        border-bottom: 1px solid var(--vt-c-text-dark-2);
+    }
+    .header-bar .logo{
+        height:80%;
+        padding:.3rem 2rem;
+    }
+    .header-nav{
+        height: 100%;
+        left:40%;
+        transform: translateX(-50%);
+        
+    }
+    .header-nav .header-link{
+        display: inline-block;
+        color:var(--vt-c-text-light-2);
+        padding:1rem 2rem;
+    }
+    
+    .header-nav  .router-link-active{
+        border-bottom:1px solid var(--vt-c-text-light-2);
+    }
+    h1 {
+      font-weight: 500;
+      font-size: 2.6rem;
+      top: -10px;
+    }
+    
+    h3 {
+      font-size: 1.2rem;
+    }
+    
+    @media (min-width: 1024px) {
+      .greetings h1,
+      .greetings h3 {
+        text-align: left;
+      }
+    }
+    </style>
+    
