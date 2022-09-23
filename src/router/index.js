b3994625b6ddc6189d190ef22fd40b9a11d0cbb2
xiayu <xiayu19950417@163.com>
2022-09-23 10:54:00 +0800

    Adjusted some styles

--- src/router/index.js
+++ src/router/index.js
@@ -15,7 +15,14 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/ProjectsView.vue')
+      component: () => import('../views/ProjectsView.vue'),
+      children:[
+				{
+					path:"",
+					name:"search",
+					component:() => import('../views/projects/SearchView.vue')
+				},
+			]
     }
   ]
 })
--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,15 +1,15 @@
+<script setup>
+  
+</script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <main>
+    <RouterView/>
+  </main>
 </template>
 
 <style>
 @media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+
 }
 </style>
