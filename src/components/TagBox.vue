b744c7546e3d98b9da452a972a927e67aee3a161
xiayu <xiayu19950417@163.com>
2022-09-26 11:12:34 +0800

    Added drag and drop event to project card

---
+++ src/components/TagBox.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="search-box">
+        <input type="text">
+        <i class="fa fa-search fa-lg"></i>
+    </div>
+</template>
+
+<style scoped>
+    .search-box{
+        border: 1px solid var(--color-border);
+        border-radius: 5px;
+        width:25rem;
+        height: 2.5rem;
+    
+    }
+    .search-box input{
+        height:100%;
+        width: 100%;
+        border-style: none;
+        background-color: transparent;
+        padding: 0 2rem 0 1rem;
+    }
+    i{
+        position: absolute; 
+        line-height: 2.5rem;
+        right: .5rem;
+    }
+
+</style>
+    
--- src/views/projects/SearchView.vue
+++ src/views/projects/SearchView.vue
@@ -1,32 +1,38 @@
 <script setup>
-    import SearchBox from '../../components/SearchBox.vue';
-    import ProjectCard from '../../components/ProjectCard.vue'
+import SearchBox from '../../components/SearchBox.vue';
+import ProjectCard from '../../components/ProjectCard.vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const turnTo = () => {
+    router.push('/projects/detail');
+};
+
 </script>
 
 <template>
-  <main>
-    <SearchBox class="search"></SearchBox>
-    <ul class="projects-list">
-        <ProjectCard v-for="card in 6" v-bind:key="card"></ProjectCard>
-    </ul>
-
-  </main>
+    <main>
+        <SearchBox class="search"></SearchBox>
+        <ul class="projects-list">
+            <ProjectCard v-for="card in 6" v-bind:key="card" @click="turnTo"></ProjectCard>
+        </ul>
+    </main>
 </template>
 
 <style scoped>
-    .search{
-        left:50%;
-        margin-top: 6rem;
-        transform: translateX(-50%);
-    }
-    .projects-list{
-        display: flex;
-        flex-wrap: wrap;
-        align-content:space-around;
-        width: 100%;
-        padding: 3rem;
-        padding-top: 6rem;
+.search {
+    left: 50%;
+    margin-top: 6rem;
+    transform: translateX(-50%);
+}
 
-    }
+.projects-list {
+    display: flex;
+    flex-wrap: wrap;
+    align-content: space-around;
+    width: 100%;
+    padding: 3rem;
+    padding-top: 6rem;
+}
 </style>
-    
