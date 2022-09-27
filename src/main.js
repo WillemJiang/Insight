206864cc12b4433bb2d71c706d519f1fe8667160
xiayu <xiayu19950417@163.com>
2022-09-27 16:57:34 +0800

    Read json files and define them as  global variables

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import readJson from './assets/js/readJson.js'
 
 import './assets/font-awesome-4.7.0/css/font-awesome.min.css'
 import './assets/main.css'
 
-
 const app = createApp(App)
+
+// Set global variables
+readJson.apply(app, ['/json/committee.json','committee'])
+readJson.apply(app, ['/json/projects_total.json','projectsList'])
 
 app.use(router)
 
