d31f5cac6d501ce92f8df74fdb9c26ffc0961132
xiayu <xiayu19950417@163.com>
2022-10-14 15:34:35 +0800

    Added a search box in the sidebar  #14

--- src/views/projects/ProjectDetail.vue
+++ src/views/projects/ProjectDetail.vue
@@ -1,45 +1,59 @@
 <script setup>
-    import ProjectCard from "../../components/ProjectCard.vue"
-    import {ref, inject} from 'vue';
+import ProjectCard from "../../components/ProjectCard.vue"
+import {ref, inject} from 'vue';
+import SearchBox from "../../components/SearchBox.vue";
     
 
-    // nav 
-    const projects_list = inject('committee')['committees']
-    const NavShow = ref(false)
-    const NavSwitch = () => {
-        NavShow.value = !NavShow.value
+const projectList = ref([])
+const isNone = ref(false)
+const updateProjectsList = (e) => {
+    if (e.length > 0){
+        projectList.value = Object.fromEntries(e)
+        isNone.value = false
+    }else{
+        projectList.value = []
+        isNone.value = true
     }
-    // 拖拽
-    const dragItem = ref(null)
-    const list2 = ref([])
-    const canvas = ref(null)
-    const dragstart = (e, item) => {
-        dragItem.value = item;
-        canvas.value.addEventListener("dragenter", dragenter);
-        canvas.value.addEventListener("dragover", dragover);
-        canvas.value.addEventListener("dragleave", dragleave);
-        canvas.value.addEventListener("drop", drop);
-    }
-    const dragend = () => {
-        canvas.value.removeEventListener("dragenter", dragenter);
-        canvas.value.removeEventListener("dragover", dragover);
-        canvas.value.removeEventListener("dragleave", dragleave);
-        canvas.value.removeEventListener("drop", drop);
-    }
-    const dragenter = (e) => {
-        e.dataTransfer.dropEffect = "move";
-    }
-    const dragover = (e) => {
-        e.preventDefault();
-    }
-    const dragleave = (e) => {
-        e.dataTransfer.dropEffect = "none";
-    }
-    const drop = () => {
-        const code= dragItem.value;
-        list2.value.push(code);
-        dragItem.value = null;
-    }
+}
+
+// nav 
+const NavShow = ref(false)
+const NavSwitch = () => {
+    NavShow.value = !NavShow.value
+}
+
+
+// 拖拽
+const dragItem = ref(null)
+const list2 = ref([])
+const canvas = ref(null)
+const dragstart = (e, item) => {
+    dragItem.value = item;
+    canvas.value.addEventListener("dragenter", dragenter);
+    canvas.value.addEventListener("dragover", dragover);
+    canvas.value.addEventListener("dragleave", dragleave);
+    canvas.value.addEventListener("drop", drop);
+}
+const dragend = () => {
+    canvas.value.removeEventListener("dragenter", dragenter);
+    canvas.value.removeEventListener("dragover", dragover);
+    canvas.value.removeEventListener("dragleave", dragleave);
+    canvas.value.removeEventListener("drop", drop);
+}
+const dragenter = (e) => {
+    e.dataTransfer.dropEffect = "move";
+}
+const dragover = (e) => {
+    e.preventDefault();
+}
+const dragleave = (e) => {
+    e.dataTransfer.dropEffect = "none";
+}
+const drop = () => {
+    const code= dragItem.value;
+    list2.value.push(code);
+    dragItem.value = null;
+}
 
 </script>
 
@@ -53,16 +67,20 @@
         <div class="right-nav" :class="NavShow?'right-nav-active':'right-nav-hidden'"  >
             <button class="nav-switch-open" @click="NavSwitch"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>
             <button class="nav-switch-close" @click="NavSwitch"><i class="fa fa-times"></i></button>
-
+            <div class="search-box">
+                <SearchBox @search="updateProjectsList"/>
+            </div>
             <ul class="projects-list">
-                <div v-for="(project, key) in projects_list" v-bind:key="key">
+                <div v-for="(project, key) in projectList" v-bind:key="key">
                     <ProjectCard   
                     @click="turnTo(project)" 
                     @dragstart="e => dragstart(e, item)" 
                     @dragend="dragend"
                     :name="key"
                     :project_info="project"/>
+
                 </div>
+                <i class="fa fa-x" v-show="isNone">未查询到结果</i>
             </ul>
         </div>        
     </main>
@@ -128,8 +146,16 @@ main{
     font-size: 1.3rem;
     cursor: pointer;
 }
+.search-box{
+   left:50%;
+   transform: translateX(-50%);
+   margin-bottom: 1rem;
+}
+.right-nav-hidden .search-box{
+    display: none;
+}
 .projects-list{
-    max-height: 96%;
+    flex:1;
     overflow-y: auto;
 }
 </style>
