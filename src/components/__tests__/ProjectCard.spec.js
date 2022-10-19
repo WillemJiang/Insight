614a21cea7ebd878dd58497127367137464181c0
xiayu <xiayu19950417@163.com>
2022-10-19 18:46:01 +0800

    Update unit test for project-card #23

--- src/components/__tests__/ProjectCard.spec.js
+++ src/components/__tests__/ProjectCard.spec.js
@@ -7,10 +7,28 @@ test('ProjectCard.vue', () => {
   const props = {
     name: 'echarts',
     project_info:{  
-      logo: "https://apache.org/logos/res/age/default.png",
+      logo: "https://apache.org/logos/res/echarts/default.png",
       description: 'a multi-model database that enables graph and relational models built on PostgreSQL'
     }
   }
   const wrapper = mount(ProjectCard,{props: props})
+  const logo_image = wrapper.findAll('[class="logo"]')
+
+  expect(logo_image.length).toBe(1)
+  expect(wrapper.html()).toContain(logo_image[0].html())
+  expect(wrapper.text()).toContain(props.project_info.description)
+})
+
+test('ProjectCard.vue', () => {
+  const props = {
+    name: 'echarts',
+    project_info:{  
+      description: 'a multi-model database that enables graph and relational models built on PostgreSQL'
+    }
+  }
+  const wrapper = mount(ProjectCard,{props: props})
+  const logo_image = wrapper.findAll('[class="logo"]')
+  
+  expect(logo_image.length).toBe(0)
   expect(wrapper.text()).toContain(props.project_info.description)
 })
