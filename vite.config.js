e2c235e4fb5e3d6aec3a25c2cf79fd4a72a92799
xiayu <xiayu19950417@163.com>
2022-10-21 15:43:29 +0800

    Adjusted chunkSizeWarningLimit #26

--- vite.config.js
+++ vite.config.js
@@ -14,5 +14,18 @@ export default defineConfig({
   },
   test:{
     environment:'jsdom'
+  },
+  build: {
+    chunkSizeWarningLimit:1500,
+    rollupOptions: {
+      output:{
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
   }
 })
