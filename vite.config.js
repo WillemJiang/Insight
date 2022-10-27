4fd18215f18b24f2147e388255d1a6583ff14541
xiayu <xiayu19950417@163.com>
2022-10-27 10:58:47 +0800

    Removed the splitting of the echarts

--- vite.config.js
+++ vite.config.js
@@ -17,9 +17,9 @@ export default defineConfig({
     environment:'jsdom'
   },
   build: {
+    chunkSizeWarningLimit:1000,
     rollupOptions: {
     plugins: [ visualizer( ) ],
-      external:['vue'],
       output:{
         manualChunks(id) {
           if (!id.includes('node_modules')) {
@@ -33,23 +33,13 @@ export default defineConfig({
           }
           const arr = dependencyName.split('/');
       
-          // echarts is too large to be a single chunk, needed to be splitted
-          if (arr[0] == 'echarts') {
-            if (arr[2] == 'component' || arr[2] == 'coord') {
-              return '_' + arr[0] + '_' + arr[1] + '_' + arr[2];
-            }
-          
-            return '_' + arr[0];
-          }
           switch(arr[0]) {
             case '@vue':
             case 'echarts':
             case 'zrender':
               return '_' + arr[0]
-
             default :
               return '__vendor'
-              
           }
         }
       }
